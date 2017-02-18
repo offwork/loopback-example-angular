dba6d19f7ce4b6af71e8821ac3d47006c5566dc9
kerem ozdemir <keremozdemir@kerem-MacBook-Pro.local>
2017-02-18 21:00:26 +0300

    first commit

---
+++ client/js/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by keremozdemir on 18/02/2017.
+ */
+
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-example-angular
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+angular
+  .module('app', [
+    'lbServices',
+    'ui.router'
+  ])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('todo', {
+        url: '',
+        templateUrl: 'views/todo.html',
+        controller: 'TodoController'
+      });
+
+    $urlRouterProvider.otherwise('todo');
+  }]);
